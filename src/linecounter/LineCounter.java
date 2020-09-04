32e852f291450f453adb48e5f80903ba82e50727
Balazs Szanka <szankabalazs@gmail.com>
2020-09-04 10:43:34 +0200

    Some basic GUI improvements.

--- src/linecounter/LineCounter.java
+++ src/linecounter/LineCounter.java
@@ -22,6 +22,7 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
@@ -30,46 +31,53 @@ import javafx.stage.Stage;
 
 public class LineCounter extends Application implements Initializable {
 
+    private final int BG_WIDTH = 595;
+    private final int BG_HEIGHT = 355;
+
 //<editor-fold defaultstate="collapsed" desc="Setting the scene.">
     @Override
     public void start(Stage stage) throws Exception {
-        ImageView imgView = new ImageView("harold.png");
+        ImageView imgView = new ImageView("open.png");
         imgView.setFitWidth(20);
         imgView.setFitHeight(20);
+        ImageView background = new ImageView("2.jpg");
+        background.setFitWidth(BG_WIDTH);
+        background.setFitHeight(BG_HEIGHT);
         Menu file = new Menu("File");
-        MenuItem item = new MenuItem("Open Multiple Files", imgView);
+        MenuItem item = new MenuItem("Open Files", imgView);
         file.getItems().addAll(item);
         //Creating a File chooser
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Multiple Files");
         fileChooser.getExtensionFilters().addAll(new ExtensionFilter("All Files", "*.*"));
-        
         //Creating a menu bar and adding menu to it.
         MenuBar menuBar = new MenuBar(file);
-        TextArea textArea = new TextArea();
+        AnchorPane pane = new AnchorPane(background);
+        TextArea textArea = new TextArea("\n\n\n\n\t\t\t\t\t\t\t(c) Balazs Szanka\n\t\t\t\t\t\thttps://github.com/bszanka");
+        textArea.setEditable(false);
         VBox vbox = new VBox(textArea);
         vbox.setLayoutX(60);
         vbox.setLayoutY(60);
-        Group root = new Group(menuBar, vbox);
-        Scene scene = new Scene(root, 595, 355, Color.LIGHTGREY);
-        
+        Group root = new Group(pane, menuBar, vbox);
+        Scene scene = new Scene(root, BG_WIDTH, BG_HEIGHT, Color.LIGHTGREY);
         //Adding action on the menu item
         item.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
+                textArea.clear();
                 //Opening a dialog box and put the output in a list
                 List<File> output = fileChooser.showOpenMultipleDialog(stage);
                 for (int i = 0; i < output.size(); i++) {
                     try {
                         textArea.appendText(countLines(output.get(i), i));
-                        
+
                     } catch (IOException ex) {
                         Logger.getLogger(LineCounter.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 }
             }
-            
+
         });
-        
+
         stage.setTitle("PalletManager");
         stage.setScene(scene);
         stage.show();
@@ -78,33 +86,34 @@ public class LineCounter extends Application implements Initializable {
 
 //<editor-fold defaultstate="collapsed" desc="The magic happens here.">
     public String countLines(File file, int i) throws FileNotFoundException, IOException {
-        
+
         FileInputStream fileStream = new FileInputStream(file);
         InputStreamReader input = new InputStreamReader(fileStream);
         BufferedReader reader = new BufferedReader(input);
-        
+
         String line;
-        
+
         // Initializing counters
         int lineCount = 0;
-        
+
         // Reading line by line from the
         // file until a null is returned
         while ((line = reader.readLine()) != null) {
             lineCount++;
         }
-        
+
         lineCount -= 2;
         String name = file.getName();
-        name = name.substring(0,15);
+        name = name.substring(0, 15);
         String res = name + " = " + lineCount + "\n";
         return res;
-        
+
     }
 //</editor-fold>
 
     public static void main(String[] args) throws IOException {
         launch(args);
+        java.net.URL url = ClassLoader.getSystemResource("harold.png");
     }
 
     @Override
